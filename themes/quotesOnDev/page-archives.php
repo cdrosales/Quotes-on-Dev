3a020511eec216a93806606ad6e02c451e7576c0
cdrosales <cara10denise@hotmail.com>
2019-08-27 14:38:55 -0400

    style: add styles to archives page

--- themes/quotesOnDev/page-archives.php
+++ themes/quotesOnDev/page-archives.php
@@ -7,66 +7,73 @@
 
 get_header(); ?>
 
+<section class="archivesPage">
 
-        <div class="quotations">
-            <i class="fas fa-quote-left"></i>
-            <i class="fas fa-quote-right"></i>
-        </div>
+<i class="fas fa-quote-left"></i>
+
+    <div class="archives">
+
+        <div class="title"><?php the_title();?></div>
+
+        <div class="subTitles">Quote Authors</div> 
+
+          <div class="archivesAuthors">
+              <?php
+              $posts = new WP_Query( array( 
+                  'post_type' => 'post', 
+                  'post_status' => 'publish', 
+                  'posts_per_page' => -1
+              ));
+
+              if ( $posts->have_posts() ) :
+              ?>
+
+                <ul>
+                  <?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
+                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                  <?php endwhile; ?>
+                </ul>
+
+              <?php endif; ?>
+          </div>
+
+
+
+
+            <div class="subTitles">Categories</div> 
+
+              <ul class="archivesCategories">
+              <?php wp_list_categories( array(
+                  'title_li'=> __( '' ),
+                  'orderby'=> 'name',
+              )); ?> 
+              </ul>
+
+
+              <div class="subTitles">Tags</div> 
+
+              <?php 
+              echo '<div class="archivesTags">';
+
+
+              $tags = get_tags();
+              foreach ( $tags as $tag ) {
+                $tag_link = get_tag_link( $tag->term_id );
+                      
+                echo '<a href=' . $tag_link . '>';
+                echo $tag->slug . '</a>';
+              }
+              echo '</div>';
+              ?>
+    </div>
+
+
+<i class="fas fa-quote-right"></i>
+
+
+</section>
+
         
-
-        <div class="quoteData">
-            <div class="author"> - <?php the_title(); ?>,</div>
-            <div class="source"></div>
-            </div>
-
-
-
-Quote Authors
-<?php
-$posts = new WP_Query( array( 
-    'post_type' => 'post', 
-    'post_status' => 'publish', 
-    'posts_per_page' => -1
-));
-
-if ( $posts->have_posts() ) :
-?>
-
-  <ul>
-    <?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
-      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-    <?php endwhile; ?>
-  </ul>
-
-<?php endif; ?>
-
-
-
-
-Categories
-<ul class="errorCategories">
-<?php wp_list_categories( array(
-    'title_li'=> __( '' ),
-    'orderby'=> 'name',
-    'show_count' => true,
-)); ?> 
-</ul>
-
-
-Tags
-<?php 
-echo '<div class="tags">';
-
-
-$tags = get_tags();
-foreach ( $tags as $tag ) {
-	$tag_link = get_tag_link( $tag->term_id );
-        
-	echo '<a href=' . $tag_link . '>';
-	echo $tag->slug . '</a>';
-}
-echo '</div>';
- ?>
 
 
 
