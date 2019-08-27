39c690cf60b02eaaee5f18c873dcf5d0c5191d5f
cdrosales <cara10denise@hotmail.com>
2019-08-27 14:09:10 -0400

    style: add styles to about page

--- themes/quotesOnDev/page-about.php
+++ themes/quotesOnDev/page-about.php
@@ -7,14 +7,41 @@
 
 get_header(); ?>
 
-<section class="aboutPage">
+<?php
+/**
+ * The main template file.
+ *
+ * @package QOD_Starter_Theme
+ */
 
-<div class="quotations">
-    <i class="fas fa-quote-left"></i>
-    <i class="fas fa-quote-right"></i>
-</div>
-        
-   
+get_header(); ?>
+
+
+<section class="homePage">
+
+<i class="fas fa-quote-left"></i>
+
+    <div class="about">
+
+        <div class="title"><?php the_title();?></div>
+        <p>Quotes on Dev is a project site for the RED Academy Web Developer
+            Professional program. It's used to experiment with AJAX, WP API,
+            jQUERY, and other cool things.<span><i class="fas fa-smile"></i></span>
+        </p>
+        <p>This site is heavily inspired by Chris Coyler's <span class="green">Quotes on Design</span>.</p>
+
+    </div>
+
+<i class="fas fa-quote-right"></i>
+
+
 </section>
+
+        
+
+
+
+<?php get_footer(); ?>
+
 
 <?php get_footer(); ?>
